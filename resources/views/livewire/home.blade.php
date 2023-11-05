ffb8839719562c532790ee7ba2a8ee1d5d781cb0
tommyvitali <tommyvitaly@gmail.com>
2023-11-05 17:01:33 -0500

    added how it works section in home

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -58,4 +58,51 @@
         </div>
       
     </section>
+<!-- SECTION HOW IT WORKS -->
+    <section class="how">
+        <h1>HOW IT WORKS</h1>
+        <div class="how-cards">
+            <div class="card" style="width: 40%;">
+                <i class="bi bi-person-circle" style="text-align:center;padding: 25px 0;"></i>
+                
+                <div class="card-body">
+                    <h5 class="card-title" style="text-align:center;">FOR THE FANS</h5>
+                    <p class="card-text">The best way to support your favourite artists and make money at the same time is called CA-SCADE.</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><i class="bi bi-bag-fill"></i>Purchase your favourite Artist music from another fan.</li>
+                    <li class="list-group-item"><i class="bi bi-vr"></i>70% of what you paid goes to the Artist, 30% to the fan you bought it from.</li>
+                    <li class="list-group-item"><i class="bi bi-file-music-fill"></i>The music you purchase is forever yours to stream on the CA-SCADE web app.</li>
+                    <li class="list-group-item"><i class="bi bi-cash-coin"></i>When a fan buy music from your music library, you earn 30% of the selling price!</li>
+                    
+                </ul>
+                <div class="card-body" style="text-align:center;">
+                    <a class="btn btn-warning rounded-5 mx-2" aria-current="page" href="#" class="card-link">SIGN UP</a>
+                    
+                </div>
+            </div>
+            <div class="card" style="width: 40%;">
+                <i class="bi bi-person-bounding-box" style="text-align:center;padding: 25px 0;"></i>
+                
+                <div class="card-body">
+                    <h5 class="card-title" style="text-align:center;">FOR THE ARTISTS</h5>
+                    <p class="card-text">The best way to earn money with your music. Let your fans sell your music! Finally you get paid for sale, and not for stream.</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><i class="bi bi-cloud-upload"></i>Upload your music for FREE on CA-SCADE.</li>
+                    <li class="list-group-item"><i class="bi bi-cash-coin"></i>When a fan sells your music to another fan, you receive 70% and the fan 30% of the selling price</li>
+                    <li class="list-group-item"><i class="bi bi-paypal"></i>All you need is a PayPal account.</li>
+                    <li class="list-group-item">What are you waiting for?</li>
+                    
+                    
+                </ul>
+                <div class="card-body" style="text-align:center;">
+                    <a class="btn btn-light rounded-5 mx-2" style="width:192px;border:1px solid #F06D4E;" aria-current="page" href="#" class="card-link">SIGN UP as ARTIST</a>
+                    
+                </div>
+            </div>
+          
+        </div>
+        
+    </section>
 </div>
