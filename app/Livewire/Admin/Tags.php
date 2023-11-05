4d30e30a79511bf6255c4d5136f0d5ceffd0b0de
coltrida <coltrida@gmail.com>
2023-11-05 16:11:04 +0100

    inseriti inserimento e cancellazione tag nella pagina tag di admin

--- app/Livewire/Admin/Tags.php
+++ app/Livewire/Admin/Tags.php
@@ -7,6 +7,22 @@ use Livewire\Component;
 
 class Tags extends Component
 {
+
+    public $name;
+
+    public function save()
+    {
+        Tag::create([
+            'name' => $this->name
+        ]);
+        $this->name = '';
+    }
+
+    public function delete($idTag)
+    {
+        Tag::find($idTag)->delete();
+    }
+
     public function render()
     {
         return view('livewire.admin.tags', [
--- resources/views/livewire/admin/tags.blade.php
+++ resources/views/livewire/admin/tags.blade.php
@@ -1,10 +1,18 @@
 <div class="container">
-    <h2>Tags</h2>
+    <div class="d-flex justify-content-between my-3">
+        <h2>Tags</h2>
+        <form wire:submit="save" class="d-flex">
+            <input type="text" wire:model="name" class="form-control border-2">
+            <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+    </div>
+
     <table class="table">
         <thead>
         <tr>
             <th scope="col">#</th>
             <th scope="col">Name</th>
+            <th scope="col">Action</th>
         </tr>
         </thead>
         <tbody>
@@ -12,6 +20,11 @@
             <tr>
                 <th scope="row">{{$item->id}}</th>
                 <td>{{$item->name}}</td>
+                <td>
+                    <button wire:click="delete({{$item->id}})" type="button" class="btn btn-danger" style="width: 60px">
+                        <i class="bi bi-trash"></i>
+                    </button>
+                </td>
             </tr>
         @endforeach
         </tbody>
