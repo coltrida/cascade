2e2ca04025abfc8169187b644f61018521e89af0
tommyvitali <tommyvitaly@gmail.com>
2023-11-05 09:34:10 -0500

    aggiunto sezione slider e jquery

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
     <link href="{{asset('/css/style.css')}}" rel="stylesheet">
     <link href="{{asset('/css/bootstrap.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/slick.css')}}" rel="stylesheet">
+    <link href="{{asset('/css/slick-theme.css')}}" rel="stylesheet">
 
     <!-- GOOGLE FONTS -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -186,7 +188,22 @@
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
 </script>  -->
-<script src="{{asset('/js/bootstrap.bundle.js')}}">
+<script src="{{asset('/js/bootstrap.bundle.js')}}"></script>
+<script
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+$(document).ready(function(){
+  $('.new-slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+});
 </script>
 
 </body>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,9 +1,61 @@
-<div class="card text-bg-dark rounded-0 border-0">
-    <img src="{{asset('/img/top-hero.jpg')}}" class="card-img">
-    <div class="card-img-overlay">
-        <p class="card-title">THE BEST WAY FOR ARTISTS AND FANS TO EARN MONEY.</p>
-        <p class="card-text">JUST PAY AND GET PAID FOR WHAT YOU LIKE.</p>
-        <p class="card-text">NO MONTHLY FEES. IT'S FOREVER YOURS.</p>
-        <a class="btn btn-warning rounded-5 mx-2" aria-current="page" href="{{route('login')}}">LEARN MORE</a>
+<div>
+    <div class="card text-bg-dark rounded-0 border-0">
+        <img src="{{asset('/img/top-hero.jpg')}}" class="card-img">
+        <div class="card-img-overlay">
+            <p class="card-title">THE BEST WAY FOR ARTISTS AND FANS TO EARN MONEY.</p>
+            <p class="card-text">JUST PAY AND GET PAID FOR WHAT YOU LIKE.</p>
+            <p class="card-text">NO MONTHLY FEES. IT'S FOREVER YOURS.</p>
+            <a class="btn btn-warning rounded-5 mx-2" aria-current="page" href="{{route('login')}}">LEARN MORE</a>
+        </div>
     </div>
+    <section class="new-on-cascade">
+        <h1>LATEST ARRIVALS</h1>
+        <div class="new-slider">
+        <div class="new-abum">
+            <a href=""><img src="{{asset('/img/download.jpg')}}" alt="">
+            <p>Album title</p>
+            <p>Artist Name</p>
+            </a>
+        </div>
+        <div class="new-abum">
+            <a href=""><img src="{{asset('/img/forever.jpg')}}" alt="">
+            <p>Album title</p>
+            <p>Artist Name</p>
+            </a>
+        </div>
+        <div class="new-abum">
+            <a href=""><img src="{{asset('/img/hanging.jpg')}}" alt="">
+            <p>Album title</p>
+            <p>Artist Name</p>
+            </a>
+        </div>
+        <div class="new-abum">
+            <a href=""><img src="{{asset('/img/indivisible.jpg')}}" alt="">
+            <p>Album title</p>
+            <p>Artist Name</p>
+            </a>
+        </div>
+        <div class="new-abum">
+            <a href=""><img src="{{asset('/img/justme.jpg')}}" alt="">
+            <p>Album title</p>
+            <p>Artist Name</p>
+            </a>
+        </div>
+        <div class="new-abum">
+            <a href=""><img src="{{asset('/img/thegood.jpg')}}" alt="">
+            <p>Album title</p>
+            <p>Artist Name</p>
+            </a>
+        </div>
+        <div class="new-abum">
+            <a href=""><img src="{{asset('/img/unreality.jpg')}}" alt="">
+            <p>Album title</p>
+            <p>Artist Name</p>
+            </a>
+
+        </div>
+  
+        </div>
+      
+    </section>
 </div>
