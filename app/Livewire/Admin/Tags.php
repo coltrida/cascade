36d61a96e313920abc5dd0050a3189a770405f99
coltrida <coltrida@gmail.com>
2023-11-05 15:43:12 +0100

    corretto slick-theme

--- app/Livewire/Admin/Tags.php
+++ app/Livewire/Admin/Tags.php
@@ -2,12 +2,15 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Tag;
 use Livewire\Component;
 
 class Tags extends Component
 {
     public function render()
     {
-        return view('livewire.admin.tags');
+        return view('livewire.admin.tags', [
+            'tags' => Tag::orderBy('name')->get()
+        ]);
     }
 }
--- resources/views/livewire/admin/tags.blade.php
+++ resources/views/livewire/admin/tags.blade.php
@@ -1,3 +1,21 @@
-<div>
+<div class="container">
     <h2>Tags</h2>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($tags as $item)
+            <tr>
+                <th scope="row">{{$item->id}}</th>
+                <td>{{$item->name}}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
 </div>
+
+
