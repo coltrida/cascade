48a870c77f693c4703f46a41606c25a56dc9a64d
coltrida <coltrida@gmail.com>
2023-11-08 22:52:00 +0100

    inserita lista ultimi album dinamici e non piu staticamente inseriti

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -2,12 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Models\Album;
 use Livewire\Component;
 
 class Home extends Component
 {
     public function render()
     {
-        return view('livewire.home');
+        return view('livewire.home', [
+            'latestAlbum' => Album::with(['artist' => function($a){
+                $a->with('user');
+            }])->latest()->limit(7)->get()
+        ]);
     }
 }
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -11,108 +11,127 @@
     <section class="new-on-cascade">
         <h1>LATEST ARRIVALS</h1>
         <div class="new-slider">
-        <div class="new-abum">
-            <a href=""><img src="{{asset('/img/download.jpg')}}" alt="">
-            <p>Album title</p>
-            <p>Artist Name</p>
-            </a>
-        </div>
-        <div class="new-abum">
-            <a href=""><img src="{{asset('/img/forever.jpg')}}" alt="">
-            <p>Album title</p>
-            <p>Artist Name</p>
-            </a>
-        </div>
-        <div class="new-abum">
-            <a href=""><img src="{{asset('/img/hanging.jpg')}}" alt="">
-            <p>Album title</p>
-            <p>Artist Name</p>
-            </a>
-        </div>
-        <div class="new-abum">
-            <a href=""><img src="{{asset('/img/indivisible.jpg')}}" alt="">
-            <p>Album title</p>
-            <p>Artist Name</p>
-            </a>
-        </div>
-        <div class="new-abum">
-            <a href=""><img src="{{asset('/img/justme.jpg')}}" alt="">
-            <p>Album title</p>
-            <p>Artist Name</p>
-            </a>
-        </div>
-        <div class="new-abum">
-            <a href=""><img src="{{asset('/img/thegood.jpg')}}" alt="">
-            <p>Album title</p>
-            <p>Artist Name</p>
-            </a>
-        </div>
-        <div class="new-abum">
-            <a href=""><img src="{{asset('/img/unreality.jpg')}}" alt="">
-            <p>Album title</p>
-            <p>Artist Name</p>
-            </a>
+            @foreach($latestAlbum as $album)
+                <div class="new-abum">
+                    <a href=""><img src="{{asset($album->cover)}}" alt="">
+                        <p>{{$album->name}}</p>
+                        <p>{{$album->artist->user->name}}</p>
+                    </a>
+                </div>
+            @endforeach
+{{--            <div class="new-abum">
+                <a href=""><img src="{{asset('/img/forever.jpg')}}" alt="">
+                    <p>Album title</p>
+                    <p>Artist Name</p>
+                </a>
+            </div>
+            <div class="new-abum">
+                <a href=""><img src="{{asset('/img/hanging.jpg')}}" alt="">
+                    <p>Album title</p>
+                    <p>Artist Name</p>
+                </a>
+            </div>
+            <div class="new-abum">
+                <a href=""><img src="{{asset('/img/indivisible.jpg')}}" alt="">
+                    <p>Album title</p>
+                    <p>Artist Name</p>
+                </a>
+            </div>
+            <div class="new-abum">
+                <a href=""><img src="{{asset('/img/justme.jpg')}}" alt="">
+                    <p>Album title</p>
+                    <p>Artist Name</p>
+                </a>
+            </div>
+            <div class="new-abum">
+                <a href=""><img src="{{asset('/img/thegood.jpg')}}" alt="">
+                    <p>Album title</p>
+                    <p>Artist Name</p>
+                </a>
+            </div>
+            <div class="new-abum">
+                <a href=""><img src="{{asset('/img/unreality.jpg')}}" alt="">
+                    <p>Album title</p>
+                    <p>Artist Name</p>
+                </a>
+
+            </div>--}}
 
         </div>
-  
-        </div>
-      
+
     </section>
-<!-- SECTION HOW IT WORKS -->
+    <!-- SECTION HOW IT WORKS -->
     <section class="how">
         <h1>HOW IT WORKS</h1>
         <div class="how-cards">
             <div class="card" style="width: 40%;">
                 <i class="bi bi-person-circle" style="text-align:center;padding: 25px 0;"></i>
-                
+
                 <div class="card-body">
                     <h5 class="card-title" style="text-align:center;">FOR THE FANS</h5>
-                    <p class="card-text">The best way to support your favourite artists and make money at the same time is called CA-SCADE.</p>
+                    <p class="card-text">The best way to support your favourite artists and make money at the same time
+                        is called CA-SCADE.</p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><i class="bi bi-bag-fill"></i>Purchase your favourite Artist music from another fan.</li>
-                    <li class="list-group-item"><i class="bi bi-vr"></i>70% of what you paid goes to the Artist, 30% to the fan you bought it from.</li>
-                    <li class="list-group-item"><i class="bi bi-file-music-fill"></i>The music you purchase is forever yours to stream on the CA-SCADE web app.</li>
-                    <li class="list-group-item"><i class="bi bi-cash-coin"></i>When a fan buy music from your music library, you earn 30% of the selling price!</li>
-                    
+                    <li class="list-group-item"><i class="bi bi-bag-fill"></i>Purchase your favourite Artist music from
+                        another fan.
+                    </li>
+                    <li class="list-group-item"><i class="bi bi-vr"></i>70% of what you paid goes to the Artist, 30% to
+                        the fan you bought it from.
+                    </li>
+                    <li class="list-group-item"><i class="bi bi-file-music-fill"></i>The music you purchase is forever
+                        yours to stream on the CA-SCADE web app.
+                    </li>
+                    <li class="list-group-item"><i class="bi bi-cash-coin"></i>When a fan buy music from your music
+                        library, you earn 30% of the selling price!
+                    </li>
+
                 </ul>
                 <div class="card-body" style="text-align:center;">
                     <a class="btn btn-warning rounded-5 mx-2" aria-current="page" href="#" class="card-link">SIGN UP</a>
-                    <a class="btn btn-secondary rounded-5 mx-2" aria-current="page" href="#" class="card-link">EXAMPLE CASE</a>
-                    
+                    <a class="btn btn-secondary rounded-5 mx-2" aria-current="page" href="#" class="card-link">EXAMPLE
+                        CASE</a>
+
                 </div>
             </div>
             <div class="card" style="width: 40%;">
                 <i class="bi bi-person-bounding-box" style="text-align:center;padding: 25px 0;"></i>
-                
+
                 <div class="card-body">
                     <h5 class="card-title" style="text-align:center;">FOR THE ARTISTS</h5>
-                    <p class="card-text">The best way to earn money with your music. Let your fans sell your music! Finally you get paid for sale, and not for stream.</p>
+                    <p class="card-text">The best way to earn money with your music. Let your fans sell your music!
+                        Finally you get paid for sale, and not for stream.</p>
                 </div>
                 <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><i class="bi bi-cloud-upload"></i>Upload your music for FREE on CA-SCADE.</li>
-                    <li class="list-group-item"><i class="bi bi-cash-coin"></i>When a fan sells your music to another fan, you receive 70% and the fan 30% of the selling price</li>
+                    <li class="list-group-item"><i class="bi bi-cloud-upload"></i>Upload your music for FREE on
+                        CA-SCADE.
+                    </li>
+                    <li class="list-group-item"><i class="bi bi-cash-coin"></i>When a fan sells your music to another
+                        fan, you receive 70% and the fan 30% of the selling price
+                    </li>
                     <li class="list-group-item"><i class="bi bi-paypal"></i>All you need is a PayPal account.</li>
                     <li style="font-weight:bold;" class="list-group-item">LET YOUR FANS TO BE YOUR BEST SALES TOOL!</li>
-                    
-                    
+
+
                 </ul>
                 <div class="card-body" style="text-align:center;">
-                    <a class="btn btn-light rounded-5 mx-2" style="width:192px;border:1px solid #F06D4E;" aria-current="page" href="#" class="card-link">SIGN UP as ARTIST</a>
-                    <a class="btn btn-secondary rounded-5 mx-2" aria-current="page" href="#" class="card-link">EXAMPLE CASE</a>
-                    
+                    <a class="btn btn-light rounded-5 mx-2" style="width:192px;border:1px solid #F06D4E;"
+                       aria-current="page" href="#" class="card-link">SIGN UP as ARTIST</a>
+                    <a class="btn btn-secondary rounded-5 mx-2" aria-current="page" href="#" class="card-link">EXAMPLE
+                        CASE</a>
+
                 </div>
             </div>
-          
+
         </div>
-        
+
     </section>
     <!-- SECTION ARTIST -->
     <section class="value-artist">
         <div class="value-left">
             <div class="content-value">
-            <h1>ARE YOU AN ARTIST?</h1>
-            <h1>GIVE VALUE TO YOUR MUSIC!</h1>
+                <h1>ARE YOU AN ARTIST?</h1>
+                <h1>GIVE VALUE TO YOUR MUSIC!</h1>
                 <p>Tired of making no money on traditional streaming platforms?</p>
                 <p>Remove your music from platforms where you make no money,
                     and join Ca-scade, where for every album the fans buy
@@ -120,11 +139,12 @@
                 <p>LET YOU FANS BE YOUR SALES FORCE!</p>
                 <p>IT'S 100% FREE.</p>
                 <a class="btn btn-warning rounded-5 mx-2" aria-current="page" href="#" class="card-link">SIGN UP</a>
-                <a class="btn btn-secondary rounded-5 mx-2" aria-current="page" href="#" class="card-link">EXAMPLE CASE</a>
-            </h1>
+                <a class="btn btn-secondary rounded-5 mx-2" aria-current="page" href="#" class="card-link">EXAMPLE
+                    CASE</a>
+                </h1>
 
             </div>
-            
+
         </div>
         <div class="value-right">
             <img src="{{asset('/img/artist-money.jpg')}}" alt="">
