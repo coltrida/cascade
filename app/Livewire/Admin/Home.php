43999435041dabf200e4a9b00c84f0d4941b9269
coltrida <coltrida@gmail.com>
2023-11-09 23:23:51 +0100

    create home page admin

--- app/Livewire/Admin/Home.php
+++ app/Livewire/Admin/Home.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Album;
+use App\Models\Artist;
+use App\Models\User;
 use Livewire\Component;
 
 class Home extends Component
 {
     public function render()
     {
-        return view('livewire.admin.home');
+        $nrUser = User::utenti()->count();
+        $nrArtist = Artist::count();
+        $albums = Album::withCount('userSales')->get();
+
+        return view('livewire.admin.home', [
+            'nrUser' => $nrUser,
+            'nrArtist' => $nrArtist,
+            'nrAlbums' => $albums->count(),
+            'nrAlbumSales' => $albums->sum('user_sales_count'),
+        ]);
     }
 }
--- resources/views/livewire/admin/home.blade.php
+++ resources/views/livewire/admin/home.blade.php
@@ -1,3 +1,59 @@
-<div>
-    <h2>Home Admin</h2>
+<div class="container text-center">
+    <div class="row">
+        <div class="col">
+            <div class="p-3">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h5 class="card-title">Nr. of Users:
+                            <span class="badge text-bg-warning">
+                                {{$nrUser}}
+                            </span>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="p-3">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h5 class="card-title">Nr. of Artists:
+                            <span class="badge text-bg-warning">
+                                {{$nrArtist}}
+                            </span>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="p-3">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h5 class="card-title">Nr. albums:
+                            <span class="badge text-bg-warning">
+                                {{$nrAlbums}}
+                            </span>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="p-3">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h5 class="card-title">Nr. album sales:
+                            <span class="badge text-bg-warning">
+                                {{$nrAlbumSales}}
+                            </span>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
