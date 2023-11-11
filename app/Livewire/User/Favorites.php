c8497133f30714041a138eb63d5e76dd3af52604
coltrida <coltrida@gmail.com>
2023-11-11 21:36:45 +0100

    finita gestione dei preferiti

--- app/Livewire/User/Favorites.php
+++ app/Livewire/User/Favorites.php
@@ -2,12 +2,30 @@
 
 namespace App\Livewire\User;
 
+use App\Livewire\LettoreAudio;
+use App\Models\User;
 use Livewire\Component;
 
 class Favorites extends Component
 {
+    public $idSongInPlay;
+
+    public function playSong($idSong)
+    {
+        $this->idSongInPlay = $idSong;
+        $this->dispatch('playsong', idSong: $idSong)->to(LettoreAudio::class);
+    }
+
     public function render()
     {
-        return view('livewire.user.favorites');
+        return view('livewire.user.favorites', [
+            'songFavorites' => User::with(['favorites' => function($s){
+                $s->with(['album' => function($al){
+                    $al->with(['artist' => function($art){
+                        $art->with('user');
+                    }]);
+                }]);
+            }])->find(auth()->user()->id)->favorites
+        ]);
     }
 }
--- resources/views/livewire/user/favorites.blade.php
+++ resources/views/livewire/user/favorites.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div class="container">
     <h2>Favorites</h2>
 
     <table class="table">
@@ -6,25 +6,27 @@
         <tr>
             <th scope="col">#</th>
             <th scope="col">Name</th>
+            <th scope="col">Album</th>
+            <th scope="col">Artist</th>
             <th scope="col">Action</th>
         </tr>
         </thead>
         <tbody>
-        @foreach ($album->songs as $item)
+        @foreach ($songFavorites as $item)
             <tr>
                 <th scope="row">{{$item->id}}</th>
                 <td>{{$item->name}}</td>
+                <td>{{$item->album->name}}</td>
+                <td>{{$item->album->artist->user->name}}</td>
                 <td>
-                    @if($albumBought)
-                        <button wire:click="playSong({{ $item->id }})"
-                                class="btn btn-primary" style="width: 50px">
-                            @if($item->id == $isSongInPlay)
-                                <i class="bi bi-pause-circle"></i>
-                            @else
-                                <i class="bi bi-play-circle"></i>
-                            @endif
-                        </button>
-                    @endif
+                    <button wire:click="playSong({{ $item->id }})"
+                            class="btn btn-primary" style="width: 50px">
+                        @if($item->id == $idSongInPlay)
+                            <i class="bi bi-pause-circle"></i>
+                        @else
+                            <i class="bi bi-play-circle"></i>
+                        @endif
+                    </button>
                 </td>
             </tr>
         @endforeach
