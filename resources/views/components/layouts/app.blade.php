b3f38076e25ae58dc76192d3fdc14675fb1bc2b5
tommyvitali <tommyvitaly@gmail.com>
2023-11-12 16:18:14 -0500

    aggiunto lettero js statico

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -241,6 +241,7 @@
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
 </script>  -->
 <script src="{{asset('/js/bootstrap.bundle.js')}}"></script>
+<script src="{{asset('/js/player-script.js')}}"></script>
 <script
     src="https://code.jquery.com/jquery-3.6.0.min.js"
     integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
@@ -284,6 +285,7 @@ $(document).ready(function(){
   })
 });
 </script>
+<script src="https://use.fontawesome.com/525bb97689.js"></script>
 
 </body>
 </html>
--- resources/views/livewire/lettore-audio.blade.php
+++ resources/views/livewire/lettore-audio.blade.php
@@ -1,8 +1,18 @@
 <div>
 
         @persist('player')
-        <audio src="{{ asset($percorsoCanzoneDaSuonare) }}" autoplay controls></audio>
-        <div>TEST</div>
+        <!-- <audio src="{{ asset($percorsoCanzoneDaSuonare) }}" autoplay controls></audio> -->
+        <div class="simple-audio-player" id="simp" data-config='{"shide_top":false,"shide_btm":false,"auto_load":false}'>
+        
+            <div class="simp-playlist">
+                <ul>
+                <li><span class="simp-source" data-src="http://localhost/cascade/public/storage/songs/1.mp3" data-cover=""></span><span class="simp-desc"></span></li>
+                <li><span class="simp-source" data-src="http://localhost/cascade/public/storage/songs/2.mp3" data-cover=""></span><span class="simp-desc"></span></li>
+                <li><span class="simp-source" data-src="http://localhost/cascade/public/storage/songs/3.mp3" data-cover=""></span><span class="simp-desc"></span></li>
+                </ul>
+            </div>
+        </div>
+       
         <a style="color: white; font-size: 28px; cursor: pointer"
            aria-current="page" wire:click="addToFavorites">
             <i class="bi bi-heart-fill"></i>
